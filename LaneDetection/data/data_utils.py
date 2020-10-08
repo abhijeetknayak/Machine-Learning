6bc94e63c5e8117892d9c88d5f362b51518f7367
Abhijeet Nayak <abhijeetknayak@gmail.com>
2020-10-08 21:58:53 +0530

    Using torch dataset class to create a data loader

--- LaneDetection/data/data_utils.py
+++ LaneDetection/data/data_utils.py
@@ -3,6 +3,36 @@ import json
 import cv2
 from easydict import EasyDict as edict
 from config import *
+import torch
+from torch.utils.data import Dataset
+import glob
+
+class TuSimpleData(Dataset):
+    def __init__(self, path=None, data_list='train'):
+        self.image_list = []
+        self.data_path = path
+
+    def __len__(self):
+        return len(self.image_list)
+
+    def __getitem__(self, idx):
+        image = cv2.imread()
+        pass
+
+    def load_json_data(self):
+        for file in glob.glob("/*.csv"):
+            with open(file) as json_file:
+                for line in json_file:
+                    data = edict(json.loads(line))
+
+                img = cv2.imread(path + data.raw_file)
+
+                h_samples = data.h_samples
+                lanes = data.lanes
+                img = np.zeros(img.shape)
+                lane_visualization(img, h_samples, lanes)
+
+
 
 def lane_visualization(img, h_samples, lanes):
     lane_viz = [[(x, y) for (x, y) in zip(lane, h_samples) if x >= 0] for lane in lanes]
@@ -11,18 +41,7 @@ def lane_visualization(img, h_samples, lanes):
     cv2.imshow("Image", img)
     cv2.waitKey(0)
 
-def load_json_data(path, name):
-    with open(path + name) as json_file:
-        data = json_file.readline()
-        data = edict(json.loads(data))
 
-        img = cv2.imread(path + data.raw_file)
-
-        h_samples = data.h_samples
-        lanes = data.lanes
-
-        img = np.zeros(img.shape)
-        lane_visualization(img, h_samples, lanes)
 
 def read_data():
     path = r"D:/TuSimple/train_set/"
