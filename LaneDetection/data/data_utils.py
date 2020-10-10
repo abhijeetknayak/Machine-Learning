ab72629f44cfe1606810bc76f9a3cdac324d1a87
Abhijeet Nayak <abhijeetknayak@gmail.com>
2020-10-10 15:36:07 +0530

    Completed data loader

--- LaneDetection/data/data_utils.py
+++ LaneDetection/data/data_utils.py
@@ -1,8 +1,9 @@
 import numpy as np
 import json
 import cv2
+import os
 from easydict import EasyDict as edict
-from config import *
+from LaneDetection.config import *
 import torch
 from torch.utils.data import Dataset
 import glob
@@ -10,28 +11,36 @@ import glob
 class TuSimpleData(Dataset):
     def __init__(self, path=None, data_list='train'):
         self.image_list = []
+        self.gt = []
         self.data_path = path
+
+        self.load_json_data()
 
     def __len__(self):
         return len(self.image_list)
 
     def __getitem__(self, idx):
-        image = cv2.imread()
-        pass
+        image = cv2.imread(self.data_path + self.image_list[idx])
+        mask = np.zeros(image.shape)
+        for lane in self.gt[idx]:
+            cv2.polylines(mask, np.int32([lane]), isClosed=False, color=(255, 255, 255), thickness=5)
+
+        return image, mask
 
     def load_json_data(self):
-        for file in glob.glob("/*.csv"):
-            with open(file) as json_file:
-                for line in json_file:
-                    data = edict(json.loads(line))
+        for file in os.listdir(self.data_path):
+            if file.endswith('.json'):
+                with open(self.data_path + file) as json_file:
+                    for line in json_file:
+                        data = edict(json.loads(line))
+                        self.image_list.append(data.raw_file)
+                        h_samples = data.h_samples
+                        lanes = data.lanes
+                        self.gt.append(self.ground_truth(h_samples, lanes))
 
-                img = cv2.imread(path + data.raw_file)
-
-                h_samples = data.h_samples
-                lanes = data.lanes
-                img = np.zeros(img.shape)
-                lane_visualization(img, h_samples, lanes)
-
+    def ground_truth(self, h_samples, lanes):
+        lane_viz = [[(x, y) for (x, y) in zip(lane, h_samples) if x >= 0] for lane in lanes]
+        return lane_viz
 
 
 def lane_visualization(img, h_samples, lanes):
@@ -41,12 +50,10 @@ def lane_visualization(img, h_samples, lanes):
     cv2.imshow("Image", img)
     cv2.waitKey(0)
 
-
-
-def read_data():
-    path = r"D:/TuSimple/train_set/"
-    label_data = r"label_data_0531.json"
-    load_json_data(path, label_data)
-
 if __name__ == '__main__':
-    read_data()
+    data = TuSimpleData(path=r"D:/TuSimple/train_set/")
+
+    img, label = data.__getitem__(10)
+    cv2.imshow("Image", img)
+    cv2.imshow("Mask", label)
+    cv2.waitKey(0)
