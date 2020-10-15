c824bdb3066e180b575463a871f077d52323f5b9
Abhijeet Nayak <abhijeetknayak@gmail.com>
2020-10-15 17:25:01 +0530

    Train function optimization

--- LaneDetection/train_net.py
+++ LaneDetection/train_net.py
@@ -6,6 +6,7 @@ from torch.utils.data import DataLoader
 from LaneDetection.config import config
 from LaneDetection.data.data_utils import TuSimpleData
 from LaneDetection.model.erfnet import Erfnet
+from LaneDetection.visualize import *
 import cv2
 import os
 
@@ -27,9 +28,15 @@ def soft_dice_loss(scores, labels):
     scores_sum_sqr = torch.sum(torch.square(scores))
     labels_sum_sqr = torch.sum(torch.square(labels))
 
-    loss = 1 - torch.div(union, torch.add(scores_sum_sqr, labels_sum_sqr))
+    loss = torch.sub(1, torch.div(union, torch.add(scores_sum_sqr, labels_sum_sqr)))
 
     return loss
+
+def mseloss(scores, labels):
+    loss = torch.mean(torch.square(torch.sub(scores, labels)))
+
+    return loss
+
 
 def save_checkpoint(state, iteration, filename='checkpoint.pth'):
     if not os.path.exists('trained'):
@@ -37,32 +44,14 @@ def save_checkpoint(state, iteration, filename='checkpoint.pth'):
     filename = 'trained/' + str(iteration) + filename
     torch.save(state, filename)
 
-if __name__ == '__main__':
-    use_cuda = torch.cuda.is_available()
-    device = torch.device("cuda:0" if use_cuda else "cpu")
-    torch.backends.cudnn.benchmark = True
-
+def train(model, train_path, max_epochs=100, print_every=10, save_every=100, save_image=50):
     params = {'batch_size': 1,
               'shuffle': True,
-              'num_workers': 6}
-
-    max_epochs = 100
-    print_every = 10
-    save_every = 100
-
+              'num_workers': 1}
     dtype = torch.float32
-    training_set = TuSimpleData(path=r"D:/TuSimple/train_set/", num_classes=5)
+
+    training_set = TuSimpleData(path=train_path, num_classes=5)
     training_generator = torch.utils.data.DataLoader(training_set, **params)
-
-    model = torch.load('trained/800checkpoint.pth')
-
-    # model = Erfnet(5)
-    # initialize_weights(model, 'kaiming')
-
-    optimizer = define_optimizer(model.parameters(), type='adam', lr=1e-3, weight_decay=0.001)
-    criterion = torch.nn.NLLLoss2d()
-
-    model.to(device)
 
     for epoch in range(max_epochs):  # Number of epochs
         for t, (images, labels) in enumerate(training_generator):
@@ -70,19 +59,44 @@ if __name__ == '__main__':
             model.train()  # Train mode
 
             scores = model(images)
-            # loss = F.binary_cross_entropy_with_logits(scores, labels)
             loss = soft_dice_loss(scores, labels)
+            # loss = mseloss(scores, labels)
+            # loss = torch.add(loss1, loss2)
             optimizer.zero_grad()
             loss.backward()
 
             optimizer.step()
 
+            if t % save_image == 0:
+                visualize_result(scores, 0.5, t, epoch)
+
             if t % print_every == 0:
-                print("Iteration {}; Loss : {}".format(t, loss))
+                print("Epoch : {}; Iteration {}; Loss : {}".format(epoch, t, loss))
 
             if t % save_every == 0:
                 save_checkpoint({'epoch': epoch + 1,
-                                 'state_dict': model.state_dict(),
+                                 'model_state_dict': model.state_dict(),
+                                 'optimizer_state_dict': optimizer.state_dict(),
+                                 'loss': loss
                                  }, t)
 
+if __name__ == '__main__':
+    use_cuda = torch.cuda.is_available()
+    device = torch.device("cuda:0" if use_cuda else "cpu")
+    torch.backends.cudnn.benchmark = True
 
+    checkpoint = torch.load('trained/0checkpoint.pth')
+
+    model = Erfnet(5)
+    # model.load_state_dict(checkpoint['model_state_dict'])
+    optimizer = define_optimizer(model.parameters(), type='adam', lr=1e-3, weight_decay=0.001)
+
+    # optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+
+    print("Starting from epoch {}".format(checkpoint['epoch']))
+
+    model.to(device)
+
+    train_path = r"C:/Users/NAYAKAB/Desktop/dataset/"
+    # train_path = r"D:/TuSimple/train_set/"
+    train(model, train_path, max_epochs=10000, save_image=100)
